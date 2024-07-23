5b840362882f2df12d45086a1c4247ae1adf1072
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-07-23 07:58:11 -0300

    feat: add configurations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,22 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"listtasks-c0977","appId":"1:333423519125:web:0c9694fa96fcdba724829c","storageBucket":"listtasks-c0977.appspot.com","apiKey":"AIzaSyB9Rc0wpq_w_U7hwJ8s1QePEaxW68Nz9nU","authDomain":"listtasks-c0977.firebaseapp.com","messagingSenderId":"333423519125","measurementId":"G-M420BG2H99"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'listtasks-c0977',
+          appId: '1:333423519125:web:0c9694fa96fcdba724829c',
+          storageBucket: 'listtasks-c0977.appspot.com',
+          apiKey: 'AIzaSyB9Rc0wpq_w_U7hwJ8s1QePEaxW68Nz9nU',
+          authDomain: 'listtasks-c0977.firebaseapp.com',
+          messagingSenderId: '333423519125',
+          measurementId: 'G-M420BG2H99',
+        })
+      )
+    ),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };
